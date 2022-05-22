925630e04fe0f88a8591231616682c56cf4e2783
Jiwoo Lee <byjiwoolee@gmail.com>
2022-05-22 18:11:53 +0900

    minor change

--- dbfs.py
+++ dbfs.py
@@ -30,7 +30,7 @@ def demand_based_function(t, C, D, T):
 def max_num_requests(t, ts, D, T):
   return math.floor((min(ts, t-D)-ts+D-1)/T) + 1 if t-D >= 0 else 0
 
-def demand_based_function_CO(t, ts, CHi, D_tight, T):
+def demand_based_function_CO(t, ts, CLo, CHi, D_tight, T):
   return CHi * max_num_requests(t, ts, D_tight, T)
 
 def demand_based_function_UN(t, ts, CLo, D_tight, T):
@@ -43,10 +43,10 @@ def total_dbf_LO(ts, task_set):
   return sum([demand_based_function(ts, CLo, D_tight, T) for (CLo, _, D_tight, _, T, _) in task_set])
 
 def total_dbf_CO(t, ts, task_set):
-  return sum([demand_based_function_CO(t, ts, CHi, D_tight, T) if L == gt.Level.HI else 0 for (_, CHi, D_tight, _, T, L) in task_set])
+  return sum([demand_based_function_CO(t, ts, CLo, CHi, D_tight, T) if L == gt.Level.HI else 0 for (CLo, CHi, D_tight, _, T, L) in task_set])
 
 def total_dbf_UN(t, ts, task_set):
-  return sum([demand_based_function_CO(t, ts, CLo, D_tight, T) if L == gt.Level.LO else 0 for (CLo, _, D_tight, _, T, L) in task_set])
+  return sum([demand_based_function_UN(t, ts, CLo, D_tight, T) if L == gt.Level.LO else 0 for (CLo, _, D_tight, _, T, L) in task_set])
 
 def calculate_dbf(t, ts, task_set):
   return total_dbf_HI(t, ts, task_set) + total_dbf_LO(ts, task_set) + total_dbf_CO(t, ts, task_set) + total_dbf_UN(t, ts, task_set)
