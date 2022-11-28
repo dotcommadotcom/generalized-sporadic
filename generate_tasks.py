d044741f2a8aeed3e10fc7c60fc54478bf540249
dotcommadotcom <byjiwoolee@gmail.com>
2022-11-27 22:26:20 -0600

    edit generate tasks

--- generate_tasks.py
+++ generate_tasks.py
@@ -1,6 +1,7 @@
-import random 
-import math
+import random as r
+import math as m
 from enum import Enum 
+import dbfs as dbf
 
 class Level(Enum):
     HI = 1
@@ -10,13 +11,13 @@ class task:
     def __init__(self, ID, T, C_LO, C_HI, D, L, tight_D = -1):
         """
         Args:
-                ID:         type: string. The id of the task.
-                T:          type: int. The minimum separation time of the task.
-                C_LO:    type: int. The WCET in low-criticality mode.
-                C_HI:    type: int. The WCET in high-criticality mode.
-                D:          type: int. The deadline of the task.
-                L:          type: Level. The criticality level of the task. Either HI (1) or LO (2).
-                tight_D:    type: int. The tightened deadline of the task. If not assigned, the same as D.
+                ID:         type: int. ID of task.
+                T:          type: int. Minimum inter-arrival separation time of task.
+                C_LO:       type: int. WCET in low-criticality mode.
+                C_HI:       type: int. WCET in high-criticality mode. If L == LO, the same as C_LO
+                D:          type: int. Relative deadline of task.
+                L:          type: Level. Criticality level of task. Either HI or LO.
+                tight_D:    type: int. Tightened deadline of the task. If not assigned, the same as D.
         """
         self.ID = ID
         self.T = T
@@ -26,77 +27,75 @@ class task:
         self.L = L
         self.tight_D = D if tight_D == -1 else tight_D
 
-class task_generation:
-    def generate_kato_utilizations(self, target_value, min_value, max_value):
-        '''
-            This function is modified from the function gen_kato_utilizations in class simso.generator.task_generator.
-            Inputs: 
-                target_value:         type: float. The target sum of the utilization ratio.
-                min_value:            type: float. The minimum utilization ratio.
-                max_value:            type: float. The maximum utilization ratio.
-            Outputs:
-                values:               A set of utilization ratios whose sum is the target_val.
-        '''
-        values = []
-        total_value = 0
-        # Classic UUniFast algorithm:
-        while total_value < target_value:
-            val = random.uniform(min_value, max_value)
-            if val + total_value> target_value:
-                val = target_value - total_value
-            total_value += val
-            values.append(val)
-        return values
+def generate_kato_utilizations(target_u, min_u, max_u):
+    '''
+        This function is modified from the function gen_kato_utilizations in class simso.generator.task_generator.
+        Inputs: 
+            target_u:               type: float. Target sum of the utilization ratio.
+            min_u:                  type: float. Minimum utilization ratio.
+            max_u:                  type: float. Maximum utilization ratio.
+        Outputs:
+            A set of utilization ratios whose sum is the target_u.
+    '''
+    utilizations = []
+    total_u = 0
+    # Classic UUniFast algorithm:
+    while total_u < target_u:
+        current_u = r.uniform(min_u, max_u)
+        if current_u + total_u > target_u:
+            current_u = target_u - total_u
+        total_u += current_u
+        utilizations.append(current_u)
+    return utilizations
 
-    def generate_task(self, utilization, ID):
-        '''
-            Generates the task based on the utilization ratio
-            How do we define the utilization ratio? --> WCET_LO / T based on Easwaran 2013
-            Inputs:
-                utilization:        type: float. The utilization ratio of the task.
-            Output:    
-                A task with all parameters initialized.
-        '''
-        C_LO = self.generate_C_LO()
-        T = self.generate_T(C_LO, utilization)
+def generate_L():
+    return r.choice([Level.HI, Level.LO])
 
-        # the operations below randomly generate T and then calculates C_LO
-        # this may cause the C_LO to be 0
-        # T = self.generate_T()
-        # C_LO = math.floor(T * utilization)
+def generate_C_LO():
+    return r.randrange(5, 31)
 
-        L = self.generate_L()
-        C_HI = C_LO if L == Level.LO else self.generate_C_HI(C_LO)
-        D = self.generate_D(C_HI)
+def generate_C_HI(C_LO, L):
+    return C_LO if L == Level.LO else C_LO * r.randrange(2, 5)
 
-        return task(ID, T, C_LO, C_HI, D, L)
+def generate_D(C_HI):
+    return r.randrange(C_HI, 301)
 
-    def generate_C_LO(self):
-        return random.randrange(5, 30)
+def generate_T(C_LO, utilization):
+    return m.ceil(C_LO / utilization) # TODO should it be less C_HI?
 
-    def generate_C_HI(self, C_LO):
-      return C_LO * random.randrange(2, 5)
+def generate_task(utilization, ID):
+    '''
+        Generates the task based on the utilization ratio
+        How do we define the utilization ratio? --> WCET_LO / T based on Easwaran 2013
+        Inputs:
+            utilization:        type: float. The utilization ratio of the task.
+        Output:    
+            A task with all parameters initialized.
+    '''
+    L = generate_L()
+    C_LO = generate_C_LO()
+    C_HI = generate_C_HI(C_LO, L)
+    D = generate_D(C_HI)
+    T = generate_T(C_LO, utilization)
 
-    def generate_T(self, C_LO, utilization):
-      return math.ceil(C_LO / utilization)
+    return task(ID, T, C_LO, C_HI, D, L)
 
-    def generate_D(self, C_HI):
-      # return random.randrange(C_HI, 101)
-      # enlarge the random range to fit in large C_HI values
-      return random.randrange(C_HI, 301)
+def generate_task_set(utilization_sum, min_u, max_u):
+    utilizations = generate_kato_utilizations(utilization_sum, min_u, max_u)
+    return [generate_task(u, i) for u, i in zip(utilizations, range(len(utilizations)))]
 
-    def generate_L(self):
-      return random.choice([Level.HI, Level.LO])
 
-    def create_task_set(self, utilization_sum, min_u, max_u):
-        utilization_ratios = self.generate_kato_utilizations(utilization_sum, min_u, max_u)
-        tasks = []
-        counter = 0
-        for u in utilization_ratios:
-            t = self.generate_task(u, str(counter))
-            while t.C_LO == 0:
-                t = self.generate_task(u, str(counter))
-            tasks.append(t)
-            counter += 1
-        return tasks
 
+
+
+def print_task_set(task_set):
+  for t in task_set:
+    print("ID: {}, C_LO: {}, C_HI: {}, T: {}, D: {}, L: {}".format(t.ID, t.C_LO, t.C_HI, t.T, t.D, t.L))
+
+if __name__ == '__main__':
+  for u_sum in [0.5, 0.6, 0.7, 0.8, 0.9]:
+    task_set = generate_task_set(u_sum, 0.02, 0.25)
+    print("------- Current utilization sum: {}---------".format(u_sum))
+    print_task_set(task_set)
+    print("Real utilization sum: {}".format(sum([task.C_LO / task.T for task in task_set])))
+    print("t_max: {}".format(dbf.calculate_t_max(task_set)))
--- test_generate_tasks.py
+++ test_generate_tasks.py
@@ -1,97 +1,105 @@
-from generate_tasks import task, task_generation
+import pytest
+import generate_tasks as gt 
+import random as r
 import dbfs as dbf
 
-def print_task_set(task_set):
-  for t in task_set:
-    print("ID:{}, WCET_LO:{}, WCET_HI:{}, T:{}, D:{}, L:{}".format(t.ID, t.WCET_LO, t.WCET_HI, t.T, t.D, t.L))
+@pytest.fixture
+def u_min():
+  return 0.02
 
-def cal_real_u_sum(task_set):
-  sum = 0
-  for t in task_set:
-    sum += t.WCET_LO/t.T
-  return sum
+@pytest.fixture
+def u_max():
+  return 0.25
 
-if __name__ == '__main__':
-  G = task_generation()
-  for u_sum in [0.5, 0.6, 0.7, 0.8, 0.9]:
-    task_set = G.create_task_set(u_sum, 0.02, 0.25)
-    print("------- Current utilization sum: {}---------".format(u_sum))
-    print_task_set(task_set)
-    print("Real utilization sum: {}".format(cal_real_u_sum(task_set)))
-    print("t_max: {}".format(dbf.calculate_t_max(task_set)))
+@pytest.fixture
+def u_sum():
+  return r.choice([0.5, 0.6, 0.7, 0.8, 0.9])
 
-'''
-# the unit tests of the prebious generate_tasks.py
-import pytest
-import generate_tasks as gt
-import random as r
+@pytest.fixture
+def seed():
+  return r.seed(80)
 
-def test_min_sep_between_5_and_100():
-  assert gt.generate_T() >= 5 and gt.generate_T() <= 100
+@pytest.fixture
+def task(u_sum, u_min, u_max):
+  utilization = r.choice(gt.generate_kato_utilizations(u_sum, u_min, u_max))
+  return gt.generate_task(utilization, "")
 
-def test_LO_computation_time_is_integer():
-  assert isinstance(gt.generate_C_Lo(5), int)
+def test_canary():
+  assert True
 
-def test_LO_computation_time_for_5_between_1_and_2():
-  assert gt.generate_C_Lo(5) >= 1 and gt.generate_C_Lo(5) <= 2
+def test_utilizations_greater_than_0_exclusive(u_sum, u_min, u_max):
+  assert min(gt.generate_kato_utilizations(u_sum, u_min, u_max)) > 0
 
-def test_LO_computation_time_for_100_between_2_and_25():
-  assert gt.generate_C_Lo(100) >= 2 and gt.generate_C_Lo(100) <= 25
+def test_utilizations_less_than_umax_inclusive(u_sum, u_min, u_max):
+  assert max(gt.generate_kato_utilizations(u_sum, u_min, u_max)) < u_max + 0.00001
 
-def test_HI_computation_time_for_13_between_26_and_52():
-  assert gt.generate_C_Hi(13) >= 26 and gt.generate_C_Hi(13) <= 52
-
-def test_deadline_for_34_between_34_and_100():
-  assert gt.generate_D(34) >= 34 and gt.generate_D(34) <= 100
+def test_utilizations_sum_equal_target_sum(u_sum, u_min, u_max):
+  assert sum(gt.generate_kato_utilizations(u_sum, u_min, u_max)) == pytest.approx(u_sum)
 
 def test_level_HI_or_LO():
   assert isinstance(gt.generate_L(), gt.Level)
 
-def test_utilization_bound_in_list():
-  assert gt.generate_utilization_bound() in [0.5, 0.6, 0.7, 0.8, 0.9]
+def test_C_LO_greater_than_5_inclusive():
+  assert gt.generate_C_LO() >= 5
 
-def test_is_utilization_valid_returns_true():
-  assert gt.is_utilization_valid([1, 1, 4], [4, 4, 8], 1)
+def test_C_LO_less_than_31_exclusive():
+  assert gt.generate_C_LO() < 31
 
-def test_is_utilization_valid_returns_false():
-  assert gt.is_utilization_valid([1, 1, 4], [4, 4, 7], 1) is False
+def test_C_LO_is_13(seed):
+  assert gt.generate_C_LO() == 13
 
-def test_is_utilization_valid_error_different_lengths():
-  with pytest.raises(AssertionError, match = "Input lists must be the same length."):
-    gt.is_utilization_valid([1], [4, 4, 8], 1)
+def test_C_HI_is_C_LO_when_L_is_LO():
+  C_LO = gt.generate_C_LO()
 
-def test_calculate_utilization_is_1():
-  assert gt.calculate_utilization([1, 1, 4], [4, 4, 8]) == 1
+  assert gt.generate_C_HI(C_LO, gt.Level.LO) == C_LO
 
-def unpack_task_set(task_set):
-  CLos = []
-  CHis = []
-  Ds = []
-  Ts = []
-  Ls = []
+def test_C_HI_greater_than_2_C_LO_inclusive():
+  C_LO = gt.generate_C_LO()
 
-  for task in task_set:
-    CLo, CHi, _, D, T, L = task
-    CLos.append(CLo)
-    CHis.append(CHi)
-    Ds.append(D)
-    Ts.append(T)
-    Ls.append(L)
-    
-  return CLos, CHis, Ds, Ts, Ls
+  assert gt.generate_C_HI(C_LO, gt.Level.HI) >= 2 * C_LO
 
-def test_create_task_set_utilization_less_than_1():
-  _, CHis, _, Ts, _ = unpack_task_set(gt.create_task_set())
+def test_C_HI_less_than_5_C_LO_exclusive():
+  C_LO = gt.generate_C_LO()
+  
+  assert gt.generate_C_HI(C_LO, gt.Level.HI) < 5 * C_LO
 
-  assert gt.is_utilization_valid(CHis, Ts, 1)
+def test_C_HI_is_90(seed):
+  assert gt.generate_C_HI(30, gt.Level.HI) == 90
 
-def test_create_task_set_CHi_less_than_deadline():
-  _, CHis, Ds, _, _ = unpack_task_set(gt.create_task_set())
+def test_D_greater_than_C_HI_inclusive():
+  C_HI = gt.generate_C_HI(gt.generate_C_LO(), gt.Level.HI)
+  
+  assert gt.generate_D(C_HI) >= C_HI
 
-  assert [CHi <= D for (CHi, D) in zip(CHis, Ds)]
+def test_D_less_than_301_exclusive():
+  C_HI = gt.generate_C_HI(gt.generate_C_LO(), gt.Level.HI)
+  
+  assert gt.generate_D(C_HI) < 301
 
-def test_create_task_set_CHi_less_than_min_separation():
-  _, CHis, _, Ts, _ = unpack_task_set(gt.create_task_set())
+def test_T_minimum_is_20():
+  assert gt.generate_T(5, 0.25) == 20
 
-  assert [CHi <= T for (CHi, T) in zip(CHis, Ts)]
-'''
+def test_T_maximum_known_is_1500():
+  assert gt.generate_T(30, 0.02) == 1500
+
+def test_task_C_LO_less_than_C_HI_inclusive(task):
+  assert task.C_LO < task.C_HI if task.L == gt.Level.HI else task.C_LO == task.C_HI
+
+def test_task_C_HI_less_than_T_inclusive(task):
+  assert task.C_HI <= task.T 
+
+def test_task_max_C_HI_equal_min_T(task):
+  task.C_HI = 4 * task.C_LO
+  task.T = gt.generate_T(task.C_LO, 0.25)
+
+  assert task.C_HI == task.T
+
+def test_task_set_not_empty(u_sum, u_min, u_max):
+  assert len(gt.generate_task_set(u_sum, u_min, u_max)) > 0
+
+def test_task_set_utilization_approximately_u_sum(u_sum, u_min, u_max):
+  task_set = gt.generate_task_set(u_sum, u_min, u_max)
+
+  assert u_sum - sum([task.C_LO / task.T for task in task_set]) < 0.05
+
+# TODO test tightened deadlines
