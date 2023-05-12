3b4aa1daeff2489cb8c8aedaeee2dd9063cb45c3
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-11 20:16:58 -0500

    edit dbfs

--- dbfs.py
+++ dbfs.py
@@ -1,35 +1,41 @@
-
 from functools import lru_cache
 import generate_tasks as gt
+import schedulability as s
 
-@lru_cache(maxsize=None)
+CACHE_SIZE = 10000
+
+def set_cache_size(length, t_max):
+  global CACHE_SIZE
+  CACHE_SIZE = length * t_max * t_max
+
+@lru_cache(maxsize=CACHE_SIZE)
 def max_requests(T, upper, lower):
   return max(0, (upper - lower)//T + 1)
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=CACHE_SIZE)
 def lower(task, ts):
   return task.T * max_requests(task.T, ts, task.tight_D) 
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=CACHE_SIZE)
 def upper_CO(task, t, ts):
   return t - task.D - task.T * max_requests(task.T, t - ts, task.D)
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=CACHE_SIZE)
 def upper_UN(task, t, ts):
   return min(ts, t - task.tight_D)
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=CACHE_SIZE)
 def demand_based_function(C, T, upper, lower):
   return  C * max_requests(T, upper, lower)
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=CACHE_SIZE)
 def demand_based_function_LO(task, ts):
   return demand_based_function(task.C_LO, task.T, upper = ts, lower = task.tight_D)
 
 def demand_based_function_HI(task, t, ts):
   return demand_based_function(task.C_HI, task.T, upper = t - task.D, lower = ts)
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=CACHE_SIZE)
 def demand_based_function_CO(task, t, ts):
   return demand_based_function(task.C_HI, task.T, upper_CO(task, t, ts), lower(task, ts))
 
@@ -38,7 +44,14 @@ def demand_based_function_UN(task, t, ts):
 
   return int(min(task.C_LO, ts - lower(task, ts) - (k - 1) * task.T / 2) * k)
 
+def sum_dbf_LO(task_set, ts):
+  set_cache_size(len(task_set), s.calculate_t_max(task_set))
+
+  return [demand_based_function_LO(task, ts) for task in task_set]
+
 def sum_dbf(task_set, t, ts):
+  set_cache_size(len(task_set), s.calculate_t_max(task_set))
+
   demand_sum = 0
   for task in task_set:
     if task.L == gt.Level.HI:
--- test_dbfs.py
+++ test_dbfs.py
@@ -11,14 +11,17 @@ def upper(): return 100
 def t(): return 1000
 
 @pytest.fixture
-def task_set():
-  r.seed(69)
+def get_seed(): return 70
+
+@pytest.fixture
+def task_set(get_seed):
+  r.seed(get_seed)
 
   return gt.generate_task_set(0.8, 0.02, 0.25)
 
 @pytest.fixture
-def LO_task(task_set):
-  r.seed(69)
+def LO_task(task_set, get_seed):
+  r.seed(get_seed)
 
   while True:
     task = r.choice(task_set)
@@ -27,8 +30,8 @@ def LO_task(task_set):
   return task
 
 @pytest.fixture
-def HI_task(task_set):
-  r.seed(69)
+def HI_task(task_set, get_seed):
+  r.seed(get_seed)
 
   while True:
     task = r.choice(task_set)
@@ -118,8 +121,8 @@ def test_max_requests_at_nT_and_n1T_minus_one_same(t):
 
 ''' TEST LOWER FUNCTION '''
 
-def test_lower_is_736(HI_task, t):
-  assert dbf.lower(HI_task, t) == 736
+def test_lower_is_869(HI_task, t):
+  assert dbf.lower(HI_task, t) == 869
 
 def test_lower_is_positive_ts_is_D(random_task):
   ''' ts >= D --> lower > 0 '''
@@ -135,8 +138,8 @@ def test_lower_is_zero_ts_less_D(random_task):
 
 ''' TEST UPPER_CO FUNCTION '''
 
-def test_upper_CO_is_439(HI_task, t):
-  assert dbf.upper_CO(HI_task, t, t // 2) == 439
+def test_upper_CO_is_425(HI_task, t):
+  assert dbf.upper_CO(HI_task, t, t // 2) == 425
  
 def test_upper_CO_less_ts(random_HI_task, t):
   ''' ∀ 0 < ts < t, upper_co < ts '''
@@ -181,7 +184,7 @@ def test_dbf_is_zero_upper_less_lower(upper):
 ''' TEST DBF_LO FUNCTION '''
 
 def test_dbf_LO_is_152(LO_task, t):
-  assert dbf.demand_based_function_LO(LO_task, t) == 152
+  assert dbf.demand_based_function_LO(LO_task, t) == 104
 
 def test_dbf_LO_is_zero_ts_less_D(random_task):
   ''' ts < tight_D --> dbf_LO == 0 '''
@@ -192,8 +195,8 @@ def test_dbf_LO_is_zero_ts_less_D(random_task):
 
 ''' TEST DBF_HI FUNCTION '''
 
-def test_dbf_HI_is_84(HI_task, t):
-  assert dbf.demand_based_function_HI(HI_task, t, t // 2) == 84
+def test_dbf_HI_is_300(HI_task, t):
+  assert dbf.demand_based_function_HI(HI_task, t, t // 2) == 300
 
 def test_dbf_HI_is_zero_t_ts_less_D(random_HI_task, t):
   ''' t - ts - D < 0 --> dbf_HI == 0 '''
@@ -205,8 +208,8 @@ def test_dbf_HI_is_zero_t_ts_less_D(random_HI_task, t):
 
 ''' TEST DBF_CO FUNCTION '''
 
-def test_dbf_CO_is_56(HI_task, t):
-  assert dbf.demand_based_function_CO(HI_task, t, t // 2) == 56
+def test_dbf_CO_is_60(HI_task, t):
+  assert dbf.demand_based_function_CO(HI_task, t, t // 2) == 60
 
 def test_dbf_CO_is_zero_t_less_D(random_HI_task):
   ''' t < D --> upper < 0 --> dbf_CO == 0 '''
@@ -217,10 +220,10 @@ def test_dbf_CO_is_zero_t_less_D(random_HI_task):
   assert dbf.demand_based_function_CO(random_HI_task, t, ts) == 0
 
 def test_dbf_CO_is_zero_ts_less_condition(random_HI_task):
-  ''' t > D and ts <= t - D - floor((t-D)/T) * T --> upper < 0 --> dbf_CO == 0 '''
+  ''' t > D and ts <= t - D - (t-D)//T * T --> upper < 0 --> dbf_CO == 0 '''
 
   t = random_HI_task.D + 100
-  ts = t - random_HI_task.D - m.floor((t - random_HI_task.D)/random_HI_task.T) * random_HI_task.T
+  ts = t - random_HI_task.D - (t - random_HI_task.D)//random_HI_task.T * random_HI_task.T
 
   assert dbf.demand_based_function_CO(random_HI_task, t, ts) == 0
 
@@ -228,35 +231,36 @@ def test_dbf_CO_is_zero_t_is_D_ts_is_Ds(random_HI_task):
   ''' t == D and ts >= tight_D --> upper == 0 and lower > 0 --> dbf_CO == 0 '''
 
   t = random_HI_task.D
-  random_HI_task.tight_D = random_HI_task.D // 2
+  random_HI_task.tight_D = r.choice([random_HI_task.D // 2, random_HI_task.D])
   ts = random_HI_task.tight_D
 
   assert dbf.demand_based_function_CO(random_HI_task, t, ts) == 0
 
-def test_dbf_CO_is_C_HI_upper_lower_is_zero(random_HI_task):
-  ''' t == D and ts < tight_D --> upper == 0 and lower == 0 --> dbf_CO == C_HI '''
+def test_dbf_CO_is_zero_up_less_low_for_ts(random_HI_task):
+  ''' t > D and ts >= (t-D)//T * T + tight_D --> upper < lower --> dbf_CO == 0 '''
 
-  t = random_HI_task.D
-  random_HI_task.tight_D = random_HI_task.D // 2
-  ts = random_HI_task.tight_D - 1
+  t = random_HI_task.D + 100
+  random_HI_task.tight_D = r.choice([random_HI_task.D // 2, random_HI_task.D])
+  ts = (t-random_HI_task.D)//random_HI_task.T * random_HI_task.T + random_HI_task.tight_D
 
-  assert dbf.demand_based_function_CO(random_HI_task, t, ts) == random_HI_task.C_HI
+  assert dbf.demand_based_function_CO(random_HI_task, t, ts) == 0
 
-def test_dbf_CO_is_zero_upper_is_t_D_lower_is_zero(random_HI_task):
-  ''' t > D and t - D < ts < tight_D --> upper == t - D and lower == 0 --> dbf_CO > 0 '''
+def test_dbf_CO_is_zero_up_less_low_for_all(random_HI_task):
+  ''' t - D - (t-D)//T * T >= tight_D --> upper < lower --> dbf_CO == 0 '''
+  # need to prove, unfinished test
+  t = random_HI_task.D + 100
+  random_HI_task.tight_D = t - random_HI_task.D - (t-random_HI_task.D)//random_HI_task.T * random_HI_task.T
+  ts = r.randint(1, t)
 
-  t = random_HI_task.D + 1
-  random_HI_task.tight_D = random_HI_task.D - 1
-  ts = r.randint(t - random_HI_task.D + 1, random_HI_task.tight_D - 1)
+  assert dbf.demand_based_function_CO(random_HI_task, t, ts) == 0
 
-  assert dbf.demand_based_function_CO(random_HI_task, t, ts) > 0
 
 
 
 ''' TEST DBF_UN FUNCTION '''
 
 def test_dbf_UN_is_38(LO_task, t):
-  assert dbf.demand_based_function_UN(LO_task, t, t // 2) == 38
+  assert dbf.demand_based_function_UN(LO_task, t, t // 2) == 26
 
 def test_dbf_UN_is_positive_upper_is_positive_lower_is_zero(random_LO_task):
   ''' upper > 0 and lower == 0 --> dbf_UN > 0 '''
@@ -282,5 +286,5 @@ def test_dbf_UN_is_zero_upper_is_zero_lower_is_positive(random_LO_task):
 
 ''' TEST SUM_DBF FUNCTION '''
 
-def test_sum_dbf_is_612(task_set, t):
-  assert dbf.sum_dbf(task_set, t, t // 2) == 512
+def test_sum_dbf_is_1211(task_set, t):
+  assert dbf.sum_dbf(task_set, t, t // 2) == 1211
