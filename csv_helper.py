2580d3b608337ab743450df297df9c6858fbac0c
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-15 18:22:05 -0500

    edit csv helper

--- csv_helper.py
+++ csv_helper.py
@@ -87,7 +87,7 @@ TOTAL_COUNT = 10000
 HEADER = ['num_tasks', 't_max', 'utilization', 'thm1', 'thm2', 'thm3', 'lo_tasks_list', 'hi_tasks_list']
 UTARGETS = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.975]
 
-if __name__ == "__main__":
-  write_target_u_to_csv(total_count =   TOTAL_COUNT, 
-                        header =        HEADER, 
-                        target_u_list = UTARGETS)
+# if __name__ == "__main__":
+#   write_target_u_to_csv(total_count =   TOTAL_COUNT, 
+#                         header =        HEADER, 
+#                         target_u_list = UTARGETS)
