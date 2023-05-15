e66f1fab7227952291e05261f712f48a44e64129
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-15 18:23:21 -0500

    edit greedy

--- greedy.py
+++ greedy.py
@@ -1,40 +1,263 @@
-import dbfs
-from generate_tasks import task
-def greedy(task_set):
-    '''
-    Inputs:   
-    task_set:       type: a list of task object. Indicates the input task set.
-    '''
-    if dbfs.schedulability_test_thm2(task_set) == False:
-        print("Not schedulable under low-criticality mode.")
-        return False
-    if dbfs.schedulability_test_thm1(task_set) == True:
-        print("Already schedualble")
-        return True
-    t_max = dbfs.calculate_t_max(task_set)
-    changed = False
-    marked = [False] * len(task_set)
-    while not changed:
-        # run schedulability tests from 0 to l for low and hi respectively
-        for j in range(0, t_max):
-            for i in range(j+1, t_max+1):
-                if dbfs.calculate_dbf(i, j, task_set) > i:
-                    # find the task that maximizes the dbf increase
-                    max_diff = 0
-                    target = -1
-                    for k in range(len(task_set)):
-                        if marked[k]:
-                            continue
-                        diff = dbfs.calculate_dbf(i, j, task_set) - dbfs.calculate_dbf(i-1, j, task_set)
-                        if diff > max_diff:
-                            max_diff = diff
-                            target = k
-                    print("Found target:{}".format(target))
-                    if target == -1:
-                        return False
-                    task_set[target].tight_D -= 1
-                    if task_set[target].tight_D == task_set[target].WCET_LO:
-                        marked[target] = True;
-                    changed = True
-    return True
+import dbfs as dbf
+from generate_tasks import Task, Task_Set
+import generate_tasks as gt
+import helper as h 
+import schedulability as s
+from functools import lru_cache
+import matplotlib.pyplot as plt
+
+
+def is_eligible(task_set: Task_Set):
+  return not task_set.thm1 and task_set.thm2
+
+def initialize_candidates(task_set: Task_Set):
+  candidates = [False] * task_set.num_tasks
+
+  for task in task_set.hi_tasks_list:
+    candidates[task.ID] = True
+
+  return candidates
+
+def find_candidates_wcet(candidates, task_set: Task_Set):
+  max_wcet_difference = 0
+  best_candidate_id = -1
+
+  for i, eligible in enumerate(candidates):
+    if eligible:
+      wcet_difference = task_set.task_set[i].C_HI - task_set.task_set[i].C_LO
+      if wcet_difference > max_wcet_difference:
+        max_wcet_difference = wcet_difference
+        best_candidate_id = task_set.task_set[i].ID
+
+  return best_candidate_id
+
+def find_candidates_min_deadline(candidates, task_set: Task_Set):
+  min_deadline = 401
+  best_candidate_id = -1
+
+  for i, eligible in enumerate(candidates):
+    if eligible and task_set.task_set[i].D < min_deadline:
+      min_deadline = task_set.task_set[i].D
+      best_candidate_id = task_set.task_set[i].ID
+
+  return best_candidate_id
+
+def get_failure_time(task_set: Task_Set, start_i = 2, start_j = 1):
+  clear_cache()
+  for i in range(start_i, task_set.t_max + 1):
+    for j in range(start_j, i):
+      if dbf.sum_dbf(task_set, i, j) > i:
+        return i, j
+  
+  return -1, -1
+
+def clear_cache():
+  return
+  # dbf.max_requests.cache_clear()
+  # dbf.lower.cache_clear()
+  # dbf.upper_CO.cache_clear()
+  # dbf.upper_UN.cache_clear()
+  # dbf.demand_based_function.cache_clear()
+  # dbf.demand_based_function_LO.cache_clear()
+  # # dbf.demand_based_function_HI.cache_clear()
+  # dbf.demand_based_function_CO.cache_clear()
+
+def greedy(task_set: Task_Set):
+  if not is_eligible(task_set):
+    return ("Not eligible for deadline-tightening", task_set)
+  
+  candidates = initialize_candidates(task_set)
+  best_candidate = -1
+  candidates_count = len(task_set.hi_tasks_list)
+  i, j = 2, 1
+
+  while candidates_count > 0: 
+    if best_candidate >= 0 and not s.schedulability_test_thm2(task_set):
+      task_set.task_set[best_candidate].tight_D += 1
+      candidates[best_candidate] = False
+      candidates_count -= 1
+
+    i, j = get_failure_time(task_set, i, j)
+    if (i, j) == (-1, -1): 
+      return (True, task_set) # sched 1 test is true now
+    
+    # best_candidate = find_candidates_wcet(candidates, task_set) 
+    best_candidate = find_candidates_min_deadline(candidates, task_set) if candidates_count > 0 else -1
+    if best_candidate < 0: return ("No more eligible candidates", task_set) 
+
+    task_set.task_set[best_candidate].tight_D -= 1 
+
+    if task_set.task_set[best_candidate].tight_D < task_set.task_set[best_candidate].C_LO:  
+      task_set.task_set[best_candidate].tight_D += 1 
+      candidates[best_candidate] = False
+      candidates_count -= 1
+
+  return ("No more eligible candidates", task_set) 
+    
+if __name__ == "__main__":
+  task_set_dict = {'num_tasks': 4, 
+                   't_max': 77, 
+                   'utilization': 0.49624662388746893, 
+                   'thm1': False, 
+                   'thm2': True, 
+                   'thm3': True, 
+                   'lo_tasks_list': [Task(0, 142, 17, 17, 63, 63)],
+                   'hi_tasks_list': [Task(1, 111, 19, 52, 58, 58), Task(2, 224, 14, 48, 284, 284), Task(3, 49, 7, 17, 162, 162)]
+                   }
+  
+  candidate_id = 3
+  task_set_eligible = Task_Set(ts_dict = task_set_dict)
+  i, j = get_failure_time(task_set_eligible, 2, 1)
+  before = dbf.sum_dbf(task_set_eligible, i, j)
+  before_sums = [dbf.sum_dbf(task_set_eligible, i, y) for y in range(i)] 
+
+  task_set_eligible.task_set[candidate_id].tight_D = task_set_eligible.task_set[candidate_id].C_LO
+  after = dbf.sum_dbf(task_set_eligible, i, j)
+  after_sums = [dbf.sum_dbf(task_set_eligible, i, y) for y in range(i)] 
+
+  print(i, j)
+  print(task_set_eligible)
+  print("demand before", before)
+
+  print("after")
+  print(task_set_eligible)
+  print("demand after", after)
+  
+
+  # hi = [dbf.sum_dbf_HI(task_set_eligible, i, y) for y in range(i)] 
+  # plt.plot(hi, label = "hi before")
+
+
+  # task_set_eligible.task_set[0].tight_D = task_set_eligible.task_set[0].C_LO
+  # task_set_eligible.task_set[3].tight_D = task_set_eligible.task_set[3].C_LO
+
+  # lo = [dbf.sum_dbf_LO(task_set_eligible, y) for y in range(i)] 
+  # # hi = [dbf.sum_dbf_HI(task_set_eligible, i, y) for y in range(i)] 
+  # co = [dbf.sum_dbf_CO(task_set_eligible, i, y) for y in range(i)] 
+  # un = [dbf.sum_dbf_UN(task_set_eligible, i, y) for y in range(i)] 
+
+
+
+
+  
+
+  plt.plot(before_sums, label = "before")
+  plt.plot(after_sums, label = "after")
+  # plt.plot(lo, label = "lo")
+  # # plt.plot(hi, label = "hi")
+  # plt.plot(co, label = "co")
+  # plt.plot(un, label = "un")
+  # plt.plot(after_sums, label = "after")
+  # plt.plot(after_sums, label = "after")
+
+  plt.legend()
+
+  plt.show()
+
+
+
+
+# def greedy_algorithm(hi_tasks, lo_tasks, t_max):
+#   post_algorithm_feasibility = False
+#   task_set = {task.ID : task for task in hi_tasks + lo_tasks}
+
+
+  
+#     # while not post_algorithm_feasibility or all(marked):
+#   for i in range(2, t_max + 1):
+#     for j in range(1, i):
+#       dbf_sum_before = dbf.sum_dbf(list(task_set.values()), i, j)
+#       if dbf_sum_before > i and any(candidates):
+#         print(i - dbf_sum_before)
+#         print("test fails ({}, {}), dbf sum {}".format(i, j, dbf_sum_before))
+#         for id in range(len(candidates)):
+#           if candidates[id]: 
+#             print("found candidate {}".format(id))
+#             break
+#         while task_set[id].tight_D > task_set[id].C_LO:
+#           task_set[id].tight_D -= 1
+#           dbf_sum_after = dbf.sum_dbf(list(task_set.values()), i, j)
+#           if dbf_sum_after <= i:
+#             print("test now succeeds ({}, {}), dbf sum {}".format(i, j, dbf_sum_after))
+#             break
+    
+#         if task_set[id].tight_D <= task_set[id].C_LO:
+#           print("removing candidate {}".format(id))
+#           candidates[id] = False
+
+#   print(candidates)
+#   h.print_task_set(list(task_set.values()))
+#   return list(task_set.values())
+
+# def try_again(task_set, t_max):
+#   for i in range(2, t_max + 1):
+#     for j in range(1, i):
+#       if dbf.sum_dbf(task_set, i, j) > i:
+#         print(i, j)
+#         return
+      
+#   print("success")
+# def greedy(task_set):
+#   '''
+#   Inputs:   
+#   task_set:       type: a list of task object. Indicates the input task set.
+#   '''
+#   # if dbf.schedulability_test_thm2(task_set) == False:
+#   #   print("Not schedulable under low-criticality mode.")
+#   #   return False
+#   # if dbf.schedulability_test_thm1_faster(task_set) == False:
+#   #   print("Not schedulable before greedy algorithm")
+
+#   t_max = dbf.calculate_t_max(task_set)
+
+#   before = dbf.sum_dbf(task_set, t_max, t_max//2)
+
+#   changed = False
+#   marked = [False] * len(task_set)
+#   # while not changed:
+#     # run schedulability tests from 0 to l for low and hi respectively
+#   for i in range(2, t_max + 1):
+#     gen = dbf.generator(i, task_set)
+#     for j in gen:
+#       if dbf.sum_dbf(task_set, i, j) > i:
+#         # find the task that maximizes the dbf increase
+#         max_diff = 0
+#         target = -1
+#         for k in range(len(task_set)):
+#           if marked[k]:
+#             continue
+#           diff = dbf.sum_dbf(task_set, i, j) - dbf.sum_dbf(task_set, i-1, j)
+#           if diff > max_diff:
+#             max_diff = diff
+#             target = k
+#         # print("Found target:{}".format(target))
+#         if target == -1:
+#           continue
+#           # return False
+#         task_set[target].tight_D -= 1
+#         if task_set[target].tight_D == task_set[target].C_LO:
+#           marked[target] = True
+#           # changed = True
+
+#   after = dbf.sum_dbf(task_set, t_max, t_max//2)
+#   return (before, after, after-before)
+#   # return dbf.schedulability_test_thm1_faster(task_set)
+#   # return True
+
+# MIN_U = 0.02
+# MAX_U = 0.25
+
+
+
+
+# # def is_best_candidate():
+
+# def greed_v2(task_set):
+#   if is_schedulable(task_set): 
+#     print("Already schedulable")
+#     return True
+#   hi_tasks = [ts for ts in task_set if ts.L == gt.Level.HI]
+#   lo_tasks = [ts for ts in task_set if ts.L == gt.Level.LO]
+#   modified_ts = greedy_algorithm(hi_tasks, lo_tasks, t_max)
+#   try_again(modified_ts, t_max)
 
--- test_greedy.py
+++ test_greedy.py
@@ -1,22 +1,218 @@
-from greedy import *
-from generate_tasks import task, task_generation
-import copy
-def print_task_set(task_set):
-  for t in task_set:
-    print("ID:{}, WCET_LO:{}, WCET_HI:{}, T:{}, D:{}, L:{}".format(t.ID, t.WCET_LO, t.WCET_HI, t.T, t.D, t.L))
+import pytest
+import random as r
 
-def cal_real_u_sum(task_set):
-  sum = 0
-  for t in task_set:
-    sum += t.WCET_LO/t.T
-  return sum
-if __name__ == '__main__':
-	G = task_generation()
-	for u_sum in [0.5, 0.6, 0.7, 0.8, 0.9]:
-		task_set = G.create_task_set(u_sum, 0.02, 0.25)
-		print("------- Current utilization sum: {}---------".format(u_sum))
-		print_task_set(task_set)
-		print("Real utilization sum: {}".format(cal_real_u_sum(task_set)))
-		temp_tasks = copy.deepcopy(task_set)
-		result = greedy(temp_tasks)
-		print("Greedy's result is: {}".format(result))
+from generate_tasks import Task, Task_Set
+import schedulability as s
+import dbfs as dbf
+import greedy as g
+
+@pytest.fixture
+def lo_task_set():
+  task_set_dict = {'num_tasks': 6, 
+                   't_max': 261, 
+                   'utilization': 0.6964358021917052, 
+                   'thm1': True, 
+                   'thm2': None, 
+                   'thm3': None, 
+                   'lo_tasks_list': [Task(0, 414, 13, 13, 300, 300), Task(3, 115, 14, 14, 267, 267), Task(5, 94, 7, 7, 172, 172)], 
+                   'hi_tasks_list': []
+                   }
+  
+  return Task_Set(ts_dict = task_set_dict)
+
+@pytest.fixture
+def task_set_not_eligible():
+  task_set_dict = {'num_tasks': 5, 
+                   't_max': 218, 
+                   'utilization': 0.4952857806657366, 
+                   'thm1': True, 
+                   'thm2': True, 
+                   'thm3': True, 
+                   'lo_tasks_list': [Task(0, 227, 6, 6, 46, 46)], 
+                   'hi_tasks_list': [Task(1, 231, 8, 30, 363, 363), Task(2, 66, 14, 36, 150, 150), Task(3, 39, 7, 21, 396, 396), Task(4, 352, 15, 55, 129, 129)]
+                   }
+  
+  return Task_Set(ts_dict = task_set_dict)
+
+@pytest.fixture
+def task_set():
+  task_set_dict = {'num_tasks': 3, 
+                   't_max': 61, 
+                   'utilization': 0.5900067674261222, 
+                   'thm1': False, 
+                   'thm2': True, 
+                   'thm3': True, 
+                   'lo_tasks_list': [Task(2, 62, 11, 11, 19, 19)],
+                   'hi_tasks_list': [Task(0, 26, 6, 23, 31, 31), Task(1, 55, 10, 27, 356, 356)]
+                   }
+  
+  return Task_Set(ts_dict = task_set_dict)
+
+@pytest.fixture
+def random_task_set():
+  return Task_Set(target_u = r.choice([0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.975]))
+
+@pytest.fixture
+def task_set_no_greedy():
+  task_set_dict = {'num_tasks': 4, 
+                  't_max': 77, 
+                  'utilization': 0.49624662388746893, 
+                  'thm1': False, 
+                  'thm2': True, 
+                  'thm3': True, 
+                  'lo_tasks_list': [Task(0, 142, 17, 17, 63, 63)],
+                  'hi_tasks_list': [Task(1, 111, 19, 52, 58, 58), Task(2, 224, 14, 48, 284, 284), Task(3, 49, 7, 17, 162, 162)]
+                  }
+  
+  return Task_Set(ts_dict = task_set_dict)
+
+''' TEST GREEDY HELPER FUNCTIONS'''
+
+def test_is_eligible_is_false(task_set_not_eligible):
+  assert g.is_eligible(task_set_not_eligible) is False
+
+def test_is_eligible_is_true(task_set):
+  assert g.is_eligible(task_set) is True
+
+def test_initialize_candidates_is_correct(task_set):
+  assert g.initialize_candidates(task_set) == [True, True, False]
+
+def test_initialize_candidates_is_equal_length(task_set):
+  candidates = g.initialize_candidates(task_set)
+
+  assert len(candidates) == len(task_set.task_set)
+
+def test_find_candidates_wcet(task_set):
+  candidates = g.initialize_candidates(task_set)
+
+  assert g.find_candidates_wcet(candidates, task_set) == 0
+
+def test_find_candidates_wcet_no_hi_tasks(lo_task_set):
+  candidates = g.initialize_candidates(lo_task_set)
+
+  assert g.find_candidates_wcet(candidates, lo_task_set) == -1
+
+def test_find_candidates_min_deadline(task_set):
+  candidates = g.initialize_candidates(task_set)
+
+  assert g.find_candidates_min_deadline(candidates, task_set) == 0
+
+def test_find_candidates_min_deadline_no_hi_tasks(lo_task_set):
+  candidates = g.initialize_candidates(lo_task_set)
+
+  assert g.find_candidates_min_deadline(candidates, lo_task_set) == -1
+
+def test_get_failure_time_not_eligible_task_set(task_set_not_eligible):
+  assert g.get_failure_time(task_set_not_eligible) == (-1, -1)
+
+def test_get_failure_time_eligible_task_set(task_set):
+  assert g.get_failure_time(task_set) == (31, 9)
+
+''' TEST GREEDY FUNCTIONALITY'''
+
+def test_greedy_not_eligible_task_set(task_set_not_eligible):
+  assert g.greedy(task_set_not_eligible) ==  ("Not eligible for deadline-tightening", task_set_not_eligible)
+
+def test_task_set_schedulable_1_is_false(task_set):
+  assert task_set.thm1 is False
+
+def test_greedy_is_True(task_set):
+  greedy_result, _ = g.greedy(task_set)
+
+  assert greedy_result is True
+
+def test_greedy_decreases_sum_dbf(task_set):
+  i, j = g.get_failure_time(task_set, 2, 1)
+  before = dbf.sum_dbf(task_set, i, j)
+  candidates = g.initialize_candidates(task_set)
+  best_candidate = g.find_candidates_wcet(candidates, task_set) 
+
+  task_set.task_set[best_candidate].tight_D = task_set.task_set[best_candidate].C_LO
+
+  assert dbf.sum_dbf(task_set, i, j) < before
+
+def test_task_set_after_greedy_failure_time_neg(task_set):
+  _, task_set_post_greedy = g.greedy(task_set)
+
+  assert g.get_failure_time(task_set_post_greedy) == (-1, -1)
+
+def test_task_set_after_greedy_decreased_tight_D(task_set):
+  _, task_set_post_greedy = g.greedy(task_set)
+
+  assert any(task.tight_D < task.D for task in task_set_post_greedy.task_set.values())
+
+def test_after_greedy_decreased_tight_D_or_not_eligible(random_task_set):
+  eligibility, random_task_set_post_greedy = g.greedy(random_task_set)
+
+  assert eligibility == "Not eligible for deadline-tightening" or any([task.tight_D < task.D for task in random_task_set_post_greedy.task_set.values()])
+
+def test_after_greedy_tight_D_greater_C_LO(random_task_set):
+  _, random_task_set_post_greedy = g.greedy(random_task_set)
+
+  assert all(task.tight_D >= task.C_LO for task in random_task_set_post_greedy.task_set.values())
+
+def test_after_greedy_sched2_is_true(random_task_set):
+  _, random_task_set_post_greedy = g.greedy(random_task_set)
+
+  assert s.schedulability_test_thm2(random_task_set_post_greedy) is True
+
+def test_greedy_fails_no_more_candidates(task_set_no_greedy):
+  greedy_result, _ = g.greedy(task_set_no_greedy)
+
+  assert greedy_result == "No more eligible candidates" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def parse_parameters(s):
+#   return (int(num) for num in re.findall(r'\d+', s))
+
+# def parse_row(ID, task_parameters):
+#   T, D, _, C_LO, C_HI, L_num = parse_parameters(task_parameters)
+
+#   L = gt.Level.HI if L_num == 1 else gt.Level.LO
+
+#   return Task(ID, T, C_LO, C_HI, D, -1,  L)
+
+# if __name__ == '__main__':
+#   filename = '/Users/jiwoo/Desktop/generalized-sporadic/output/task_sets2.csv'
+  
+#   with open(filename) as file:
+#     reader = csv.reader(file, delimiter=',')
+#     line_count = 0
+#     for row in reader:
+#       task_set = []
+#       if line_count > 0:
+#         print("-------Utilization sum: {}-------".format(row[0]))
+#         print("t_MAX: {}".format(row[1]))
+#         for i in range(2, 13):
+#           if row[i]:
+#             task_set.append(parse_row(i, row[i]))
+#         print_task_set(task_set)
+#         result = greedy(task_set)
+#         print("Greedy's result is: {}".format(result))
+#       line_count += 1
+#     print("Processed {} task sets.".format(line_count - 1))
