0ee8f343f6aab7601fe4f4980b964ba9398408b6
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-15 18:24:11 -0500

    edit schedulability

--- schedulability.py
+++ schedulability.py
@@ -1,7 +1,10 @@
+import multiprocessing as mp
+
 import dbfs as dbf
 
 def schedulability_test_thm1(task_set):
   if task_set.t_max == 0: return False
+  print(mp.current_process().name, task_set.t_max)
 
   return not any(dbf.sum_dbf(task_set, i, j) > i  for i in range(2, task_set.t_max + 1) for j in range(1, i))
 
