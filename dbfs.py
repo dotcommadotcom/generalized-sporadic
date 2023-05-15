e6a57bdbb7c7e3c810be2d3220d3a8dd5bace9a0
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-15 18:23:58 -0500

    edit dbf

--- dbfs.py
+++ dbfs.py
@@ -1,41 +1,39 @@
 from functools import lru_cache
 
-import generate_tasks as gt
-
 CACHE_SIZE = 10000
 
 def set_cache_size(length, t_max):
   global CACHE_SIZE
   CACHE_SIZE = length * t_max * t_max
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def max_requests(T, upper, lower):
   return max(0, (upper - lower)//T + 1)
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def lower(task, ts):
   return task.T * max_requests(task.T, ts, task.tight_D) 
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def upper_CO(task, t, ts):
   return t - task.D - task.T * max_requests(task.T, t - ts, task.D)
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def upper_UN(task, t, ts):
   return min(ts, t - task.tight_D)
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def demand_based_function(C, T, upper, lower):
   return  C * max_requests(T, upper, lower)
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def demand_based_function_LO(task, ts):
   return demand_based_function(task.C_LO, task.T, upper = ts, lower = task.tight_D)
 
 def demand_based_function_HI(task, t, ts):
   return demand_based_function(task.C_HI, task.T, upper = t - task.D, lower = ts)
 
-@lru_cache(maxsize=CACHE_SIZE)
+# @lru_cache(maxsize=CACHE_SIZE)
 def demand_based_function_CO(task, t, ts):
   return demand_based_function(task.C_HI, task.T, upper_CO(task, t, ts), lower(task, ts))
 
@@ -47,7 +45,7 @@ def demand_based_function_UN(task, t, ts):
 def sum_dbf_LO(task_set, ts):
   set_cache_size(task_set.num_tasks, task_set.t_max)
 
-  return sum([demand_based_function_LO(task, ts) for task in task_set.task_set])
+  return sum([demand_based_function_LO(task, ts) for task in task_set.task_set.values()])
 
 def sum_dbf_HI(task_set, t, ts):
   set_cache_size(task_set.num_tasks, task_set.t_max)
@@ -68,8 +66,8 @@ def sum_dbf(task_set, t, ts):
   set_cache_size(task_set.num_tasks, task_set.t_max)
 
   demand_sum = 0
-  for task in task_set.task_set:
-    if task.L == gt.Level.HI:
+  for task in task_set.task_set.values():
+    if task.C_LO < task.C_HI:
       demand_sum += demand_based_function_HI(task, t, ts) + demand_based_function_CO(task, t, ts)
     else:
       demand_sum += demand_based_function_UN(task, t, ts)
