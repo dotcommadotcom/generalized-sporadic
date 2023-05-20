05cde559d8c55bc93c2a283c9711f2c8336c99ff
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-20 14:25:14 -0500

    edit search algorithm

--- search_algorithm.py
+++ search_algorithm.py
@@ -1,22 +1,10 @@
+from collections import deque
+
 import dbfs as dbf
 import schedulability as s
-from generate_tasks import Task, Task_Set
-from collections import deque
 
 def is_eligible(task_set):
   return not task_set.thm1 and task_set.thm2 and task_set.thm3
-
-def find_max_wcet_difference(task_set):
-  max_wcet_difference = 0
-  best_candidate_id = -1
-
-  for task in task_set.hi_tasks_list:
-    wcet_difference = task_set.task_set[task.ID].C_HI - task_set.task_set[task.ID].C_LO
-    if wcet_difference > max_wcet_difference:
-      max_wcet_difference = wcet_difference
-      best_candidate_id = task.ID
-
-  return best_candidate_id
 
 def initialize_candidates(task_set):
   return deque([task.ID for task in sorted([task for task in task_set.hi_tasks_list], key = lambda task: task.C_HI - task.C_LO, reverse = True)])
@@ -32,14 +20,14 @@ def get_failure_time(task_set):
 def is_demand_at_minimum(T, C_LO, D, ts):
   return ts < C_LO or ts - ((ts - 1)//T) * T > D
 
-def find_optimal_tight_D(T, C_LO, prev_tight_D, ts):
+def find_optimal_tight_D(T, C_LO, previous_tight_D, ts):
   for n in range((ts - 1)//T + 1):
-    if C_LO <= ts - n * T < prev_tight_D:
+    if C_LO <= ts - n * T < previous_tight_D:
       return ts - n * T
 
-  return prev_tight_D
+  return previous_tight_D
 
-def greedy_search(task_set):
+def tighten_deadline(task_set):
   if not is_eligible(task_set):
     return ("Not eligible for deadline-tightening", task_set)
   
@@ -48,31 +36,30 @@ def greedy_search(task_set):
   t, ts = get_failure_time(task_set)
 
   while len(candidates) > 0: 
-    # find candidate
+    # Find best candidate with max(C_HI - C_LO) 
     best_candidate = candidates.popleft() if len(candidates) > 0 else -1
     if best_candidate < 0: return ("No more eligible candidates", task_set) 
 
-    # check if demand can be reduced by best candidate    
     T, C_LO, D, previous_tight_D = task_set.task_set[best_candidate].T, task_set.task_set[best_candidate].C_LO, task_set.task_set[best_candidate].D, task_set.task_set[best_candidate].tight_D
+
+    # Check if tightening best candidate deadline can reduce demand
     if is_demand_at_minimum(T, C_LO, D, ts):
       continue
 
-    # find optimal decreased tight_D
+    # Tighten best candidate deadline to ts - n * T 
     task_set.task_set[best_candidate].tight_D = find_optimal_tight_D(T, C_LO, previous_tight_D, ts)
-
-    # if no more changes possible to tight_D, continue
     if task_set.task_set[best_candidate].tight_D == previous_tight_D:
       continue
 
-    # backtrack if reducing tight_D broke thm2
-    if not s.schedulability_test_thm2(task_set) or not s.schedulability_test_thm3(task_set):
+    # Backtrack if tightening deadline causes theorem 2 or 3 to be false
+    if not (s.schedulability_test_thm2(task_set) and s.schedulability_test_thm3(task_set)):
       task_set.task_set[best_candidate].tight_D = previous_tight_D
       continue
     
-    # check if sched 1 test will now pass or if it fails at another time
+    # Check if theorem 1 is True with tightened deadline or if theorem 1 fails at another time
     t, ts = get_failure_time(task_set)
     if (t, ts) == (-1, -1): 
-      return (True, task_set) # sched 1 test is true now
+      return (True, task_set) # Task set is now schedulable with tightened deadline
     
     candidates.appendleft(best_candidate)
 
--- test_search_algorithm.py
+++ test_search_algorithm.py
@@ -9,43 +9,15 @@ import dbfs as dbf
 import search_algorithm as alg
 
 @pytest.fixture
+def random_task_set():
+  return Task_Set(target_u = 0.6)
+
+@pytest.fixture
 def random_hi_task():
   while True:
     task = generate_task(r.uniform(0.02, 0.5))
     if task.L == Level.HI:
       return task
-
-@pytest.fixture
-def random_task_set():
-  return Task_Set(target_u = 0.6)
-
-@pytest.fixture
-def task_set_thm1_true():
-  task_set_dict = {'num_tasks': 5, 
-                   't_max': 218, 
-                   'utilization': 0.4952857806657366, 
-                   'thm1': True, 
-                   'thm2': True, 
-                   'thm3': True, 
-                   'lo_tasks_list': [Task(0, 227, 6, 6, 46, 46)], 
-                   'hi_tasks_list': [Task(1, 231, 8, 30, 363, 363), Task(2, 66, 14, 36, 150, 150), Task(3, 39, 7, 21, 396, 396), Task(4, 352, 15, 55, 129, 129)]
-                   }
-  
-  return Task_Set(task_set_dict = task_set_dict)
-
-@pytest.fixture
-def task_set2():
-  task_set_dict = {'num_tasks': 4, 
-                  't_max': 77, 
-                  'utilization': 0.49624662388746893, 
-                  'thm1': False, 
-                  'thm2': True, 
-                  'thm3': True, 
-                  'lo_tasks_list': [Task(0, 142, 17, 17, 63, 63)],
-                  'hi_tasks_list': [Task(1, 111, 19, 52, 58, 58), Task(2, 224, 14, 48, 284, 284), Task(3, 49, 7, 17, 162, 162)]
-                  }
-  
-  return Task_Set(task_set_dict = task_set_dict)
 
 @pytest.fixture
 def task_set():
@@ -154,47 +126,23 @@ def test_tight_D_changes_at_ts_nT(random_hi_task):
 
   assert tight_D_list == change_list
 
-def test_demand_no_change_with_tight_D_under_conditions(random_hi_task):
+def test_demand_at_minimum_under_ts_conditions(random_hi_task):
   t = 1000
   while True:
     ts = r.randrange(1, t)
     if ts < random_hi_task.C_LO or ts - ((ts - 1)//random_hi_task.T) * random_hi_task.T > random_hi_task.D:
       break
   previous_task_demand = dbf.demand_based_function_LO(random_hi_task, ts) + dbf.demand_based_function_CO(random_hi_task, t, ts)
-  tight_task = copy.deepcopy(random_hi_task)
-  tight_task.tight_D = r.randrange(tight_task.C_LO, tight_task.D + 1)
+  demand_change_list = []
+  previous_tight_D = random_hi_task.tight_D
 
-  demand_change = dbf.demand_based_function_LO(tight_task, ts) + dbf.demand_based_function_CO(tight_task, t, ts) - previous_task_demand
+  while random_hi_task.C_LO < previous_tight_D <= random_hi_task.D:
+    tight_task = copy.deepcopy(random_hi_task)
+    tight_task.tight_D = previous_tight_D - 1
+    demand_change_list.append(dbf.demand_based_function_LO(tight_task, ts) + dbf.demand_based_function_CO(tight_task, t, ts) - previous_task_demand)
+    previous_tight_D = tight_task.tight_D 
 
-  assert demand_change == 0
-
-
-
-
-
-
-
-
-# ''' TEST EXAMPLE '''
-
-# def test_task_set_greedy_sched_tight_D_is_9(task_set):
-#   task_set.task_set[0].tight_D = 9
-
-#   assert all([s.schedulability_test_thm1(task_set), s.schedulability_test_thm2(task_set), s.schedulability_test_thm3(task_set)])
-  
-# def test_task_set2_greedy_sched_tight_D_is_9(task_set2):
-#   assert alg.initialize_candidates(task_set2) == deque([2, 1, 3])
-#   assert alg.get_failure_time(task_set2) == (63, 12)
-
-#   task_set2.task_set[3].tight_D = 12
-#   assert alg.get_failure_time(task_set2) == (58, 12)
-#   assert alg.is_demand_at_minimum(task_set2.task_set[3].T, task_set2.task_set[3].C_LO, task_set2.task_set[3].D, 12) is True
-
-#   task_set2.task_set[3].tight_D = 7
-
-#   assert all([s.schedulability_test_thm1(task_set2), s.schedulability_test_thm2(task_set2), s.schedulability_test_thm3(task_set2)])
-
-
+  assert sum(demand_change_list) == 0
   
 ''' TEST IS ELIGIBLE '''
 
@@ -218,15 +166,15 @@ def test_is_eligible_is_true(task_set):
 
   assert alg.is_eligible(task_set) is True
 
-''' TEST MAX DIFFERENCE WCET HEURISTIC '''
+# ''' TEST MAX DIFFERENCE WCET HEURISTIC '''
 
-def test_find_max_wcet_difference_is_zero(task_set):
-  assert alg.find_max_wcet_difference(task_set) == 0
+# def test_find_max_wcet_difference_is_zero(task_set):
+#   assert alg.find_max_wcet_difference(task_set) == 0
 
-def test_find_max_wcet_difference_no_candidates(task_set):
-  task_set.hi_tasks_list = []
+# def test_find_max_wcet_difference_no_candidates(task_set):
+#   task_set.hi_tasks_list = []
 
-  assert alg.find_max_wcet_difference(task_set) == -1
+#   assert alg.find_max_wcet_difference(task_set) == -1
 
 ''' TEST INITIALIZE CANDIDATES '''
 
@@ -244,29 +192,40 @@ def test_initialize_candidates_top_max_wcet_difference(random_task_set):
   assert len(random_task_set.hi_tasks_list) == 0 or \
     all(task.C_HI - task.C_LO <= random_task_set.task_set[candidates[0]].C_HI - random_task_set.task_set[candidates[0]].C_LO for task in random_task_set.hi_tasks_list)
 
-''' TEST GET FAILURE TIME  '''
+''' TEST GET FAILURE TIME '''
 
-def test_get_failure_time_thm1_true(task_set_thm1_true):
-  assert alg.get_failure_time(task_set_thm1_true) == (-1, -1)
+def test_get_failure_time_neg_thm1_true():
+  task_set_dict = {'num_tasks': 5, 
+                   't_max': 218, 
+                   'utilization': 0.4952857806657366, 
+                   'thm1': True, 
+                   'thm2': True, 
+                   'thm3': True, 
+                   'lo_tasks_list': [Task(0, 227, 6, 6, 46, 46)], 
+                   'hi_tasks_list': [Task(1, 231, 8, 30, 363, 363), Task(2, 66, 14, 36, 150, 150), Task(3, 39, 7, 21, 396, 396), Task(4, 352, 15, 55, 129, 129)]
+                   }
+  
+  task_set = Task_Set(task_set_dict = task_set_dict)
+  
+  assert alg.get_failure_time(task_set) == (-1, -1)
 
-def test_get_failure_time_task_set(task_set):
+def test_get_failure_time_pos_task_set(task_set):
   assert alg.get_failure_time(task_set) == (31, 9)
 
-''' TEST IS DEMAND AT MINIMUM  '''
+''' TEST IS DEMAND AT MINIMUM '''
 
 def test_is_demand_at_minimum_true_ts_less_CLO():
   ts = 6
   task = Task(-1, 49, 7, 17, 162, 162)
 
-  assert ts - ((ts - 1)//task.T) * task.T <= task.D
-  assert alg.is_demand_at_minimum(task.T, task.C_LO, task.D, ts) is True
+  assert ts - ((ts - 1)//task.T) * task.T <= task.D and \
+    alg.is_demand_at_minimum(task.T, task.C_LO, task.D, ts) is True
 
 def test_is_demand_at_minimum_true_ts_greater_D():
   ts = 1000
   task = Task(-1, 163, 7, 17, 21, 21)
 
-  assert ts >= task.C_LO
-  assert alg.is_demand_at_minimum(task.T, task.C_LO, task.D, ts) is True
+  assert ts >= task.C_LO and alg.is_demand_at_minimum(task.T, task.C_LO, task.D, ts) is True
 
 def test_is_demand_at_minimum_false(task_set):
   ts = 9
@@ -274,61 +233,86 @@ def test_is_demand_at_minimum_false(task_set):
 
   assert alg.is_demand_at_minimum(task.T, task.C_LO, task.D, ts) is False
 
+''' TEST FIND OPTIMAL TIGHT D '''
 
+def test_find_optimal_tight_D_is_between_C_LO_and_D(random_task_set):
+  ts = r.randrange(1, 1000)
+  id = random_task_set.hi_tasks_list[0].ID if len(random_task_set.hi_tasks_list) > 0 else random_task_set.lo_tasks_list[0].ID 
+  task = random_task_set.task_set[id]
 
-# def test_get_failure_time_task_set(task_set):
-#   assert alg.get_failure_time(task_set) == (31, 9)
+  assert task.C_LO <= alg.find_optimal_tight_D(task.T, task.C_LO, task.tight_D, ts) <= task.D
 
-# ''' TEST GREEDY FUNCTIONALITY'''
+def test_find_optimal_tight_D_is_ts_nT():
+  ts, task = 91, Task(-1, 10, 5, 20, 100, 100)
+  previous_tight_D = 100
+  tight_D_list = []
 
-# def test_greedy_not_eligible_task_set(task_set_not_eligible, default_heuristic):
-#   assert alg.greedy_search(task_set_not_eligible, default_heuristic) ==  ("Not eligible for deadline-tightening", task_set_not_eligible)
+  while True:
+    tight_D = alg.find_optimal_tight_D(task.T, task.C_LO, previous_tight_D, ts)
+    if tight_D == previous_tight_D:
+      break
+    tight_D_list.append(tight_D)
+    previous_tight_D = tight_D
 
-# def test_task_set_schedulable_1_is_false(task_set):
-#   assert task_set.thm1 is False
+  assert tight_D_list == [91, 81, 71, 61, 51, 41, 31, 21, 11]
 
-# def test_greedy_is_True(task_set, default_heuristic):
-#   greedy_result, _ = alg.greedy_search(task_set, default_heuristic)
+''' TEST SEARCH ALGORITHM '''
 
-#   assert greedy_result is True
+def test_search_not_eligible_task_set_thm2_false(random_task_set):
+  random_task_set.thm1 = True
+  random_task_set.thm2 = False
+  random_task_set.thm3 = False
+  
+  assert alg.tighten_deadline(random_task_set)[0] == "Not eligible for deadline-tightening"
 
-# def test_greedy_decreases_sum_dbf(task_set):
-#   i, j = alg.get_failure_time(task_set, 2, 1)
-#   before = dbf.sum_dbf(task_set, i, j)
-#   candidates = alg.initialize_candidates(task_set)
-#   best_candidate = alg.find_max_wcet_difference(candidates, task_set) 
+def test_search_is_True(task_set):
+  search_result, _ = alg.tighten_deadline(task_set)
 
-#   task_set.task_set[best_candidate].tight_D = task_set.task_set[best_candidate].C_LO
+  assert search_result is True
 
-#   assert dbf.sum_dbf(task_set, i, j) < before
+def test_search_no_more_eligible_candidates(random_task_set):
+  random_task_set.thm1 = False
+  random_task_set.thm2 = True
+  random_task_set.thm3 = True
+  random_task_set.hi_tasks_list = []
 
-# def test_task_set_after_greedy_failure_time_neg(task_set, default_heuristic):
-#   _, task_set_post_greedy = alg.greedy_search(task_set, default_heuristic)
+  search_result, _ = alg.tighten_deadline(random_task_set)
 
-#   assert alg.get_failure_time(task_set_post_greedy) == (-1, -1)
+  assert search_result == "No more eligible candidates"
 
-# def test_task_set_after_greedy_decreased_tight_D(task_set, default_heuristic):
-#   _, task_set_post_greedy = alg.greedy_search(task_set, default_heuristic)
+def test_search_success_decreases_sum_dbf(task_set):
+  t, ts = alg.get_failure_time(task_set)
+  before = dbf.sum_dbf(task_set, t, ts)
+  best_candidate = alg.initialize_candidates(task_set)[0]
+  T, C_LO, previous_tight_D = task_set.task_set[best_candidate].T, task_set.task_set[best_candidate].C_LO, task_set.task_set[best_candidate].tight_D
 
-#   assert any(task.tight_D < task.D for task in task_set_post_greedy.task_set.values())
+  task_set.task_set[best_candidate].tight_D = alg.find_optimal_tight_D(T, C_LO, previous_tight_D, ts)
 
-# def test_after_greedy_decreased_tight_D_or_not_eligible(random_task_set, default_heuristic):
-#   eligibility, random_task_set_post_greedy = alg.greedy_search(random_task_set, default_heuristic)
+  assert dbf.sum_dbf(task_set, t, ts) < before
 
-#   assert eligibility == "Not eligible for deadline-tightening" or any([task.tight_D < task.D for task in random_task_set_post_greedy.task_set.values()])
+def test_search_decreases_tight_D(task_set):
+  _, task_set_post_search = alg.tighten_deadline(task_set)
 
-# def test_after_greedy_tight_D_greater_C_LO(random_task_set, default_heuristic):
-#   _, random_task_set_post_greedy = alg.greedy_search(random_task_set, default_heuristic)
+  assert any(task.tight_D < task.D for task in task_set_post_search.task_set.values())
 
-#   assert all(task.tight_D >= task.C_LO for task in random_task_set_post_greedy.task_set.values())
+def test_search_success_failure_time_neg(task_set):
+  _, task_set_post_search = alg.tighten_deadline(task_set)
 
-# # sometimes produces errors
-# def test_after_greedy_sched2_is_true(random_task_set, default_heuristic):
-#   _, random_task_set_post_greedy = alg.greedy_search(random_task_set, default_heuristic)
+  assert alg.get_failure_time(task_set_post_search) == (-1, -1)
 
-#   assert s.schedulability_test_thm2(random_task_set_post_greedy) is True
+def test_search_tight_D_between_C_LO_D(random_task_set):
+  _, random_task_set_post_search = alg.tighten_deadline(random_task_set)
 
-# def test_greedy_fails_no_more_candidates(task_set_no_greedy, default_heuristic):
-#   greedy_result, _ = alg.greedy_search(task_set_no_greedy, default_heuristic)
+  assert all(task.C_LO <= task.tight_D <= task.D for task in random_task_set_post_search.task_set.values())
 
-#   assert greedy_result == "No more eligible candidates" 
+def test_search_thm2_still_true(random_task_set):
+  before_search_thm2 = s.schedulability_test_thm2(random_task_set)
+  _, random_task_set_post_search = alg.tighten_deadline(random_task_set)
+
+  assert before_search_thm2 is False or s.schedulability_test_thm2(random_task_set_post_search) is True
+
+def test_search_thm3_still_true(random_task_set):
+  before_search_thm3 = s.schedulability_test_thm3(random_task_set)
+  _, random_task_set_post_search = alg.tighten_deadline(random_task_set)
+
+  assert before_search_thm3 is False or s.schedulability_test_thm3(random_task_set_post_search) is True
