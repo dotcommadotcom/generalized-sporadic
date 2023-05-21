0915082d5ffbce7e50854c7a0d2f16da07fdc2d8
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-20 21:59:55 -0500

    update generate tasks

--- generate_tasks.py
+++ generate_tasks.py
@@ -1,35 +1,22 @@
 from enum import Enum 
-from math import gcd, ceil, floor
+from math import gcd, floor
 from functools import reduce
 import random as r
-
-MIN_U = 0.02
-MAX_U = 0.25
 
 MIN_CLO = 5
 MAX_CLO = 25
 
-MIN_CHI = 2
-MAX_CHI = 4
+MAX_D = 500
+MAX_T = 500
 
-MAX_D = 400
+TARGET_U = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
 
 class Level(Enum):
+  LO = 0
   HI = 1
-  LO = 2
-
+  
 class Task:
   def __init__(self, ID, T, C_LO, C_HI, D, tight_D = -1, L = None):
-    """
-    Args:
-      ID:         type: int. ID of task.
-      T:          type: int. Minimum inter-arrival separation time of task.
-      C_LO:       type: int. WCET in low-criticality mode.
-      C_HI:       type: int. WCET in high-criticality mode. If L == LO, same as C_LO
-      D:          type: int. Relative deadline of task.
-      L:          type: Level. Criticality level of task. Either HI or LO.
-      tight_D:    type: int. Tightened deadline of the task. If not assigned, the same as D.
-    """
     self.ID = ID
     self.T = T
     self.C_LO = C_LO
@@ -43,43 +30,64 @@ class Task:
 
   def __repr__(self):
     return "Task({}, {}, {}, {}, {}, {})".format(self.ID, self.T, self.C_LO, self.C_HI, self.D, self.tight_D)
-  
-def generate_L():
-  return r.choice([Level.HI, Level.LO])
+
+def generate_L(hi_probability = 0.5):
+  return r.choices([Level.HI, Level.LO], weights = [hi_probability, 1 - hi_probability], k = 1)[0]
 
 def generate_C_LO():
   return r.randrange(MIN_CLO, MAX_CLO)
 
-def generate_C_HI(C_LO, L):
-  return C_LO if L == Level.LO else r.randrange(C_LO * MIN_CHI, C_LO * MAX_CHI + 1)
+def generate_C_HI(C_LO, L, wcet_ratio = 4):
+  return C_LO if L == Level.LO else r.randrange(C_LO * 2, C_LO * wcet_ratio + 1)
 
 def generate_D(C_HI):
   return r.randrange(C_HI, MAX_D + 1)
 
-def generate_T(C_LO, utilization):
-  return ceil(C_LO / utilization) 
+def generate_T(C_HI):
+  return r.randrange(C_HI, MAX_T + 1)
 
-def generate_task(utilization, ID = -1):
-  L = generate_L()
+def generate_task(ID = -1, hi_probability = 0.5, wcet_ratio = 4):
+  L = generate_L(hi_probability)
   C_LO = generate_C_LO()
-  C_HI = generate_C_HI(C_LO, L)
+  C_HI = generate_C_HI(C_LO, L, wcet_ratio)
   D = generate_D(C_HI)
-  T = generate_T(C_LO, utilization)
+  T = generate_T(C_HI)
 
   return Task(ID, T, C_LO, C_HI, D, -1, L)
 
 class Task_Set:
-  def __init__(self, target_u = None, task_set_dict = None):
+  def __init__(self, target_u = None, task_set_dict = None, hi_probability = 0.5, wcet_ratio = 4):
     if target_u:
-      self.task_set = self.initialize_task_set(target_u)
-      self.num_tasks = len(self.task_set)
-      self.utilization = self.calculate_utilization()
+      self.task_set = {}
+      self.num_tasks = 0
+      self.lo_tasks_list = []
+      self.hi_tasks_list = []
+
+      while True:
+        task = generate_task(self.num_tasks, hi_probability, wcet_ratio)
+
+        self.task_set[self.num_tasks] = task
+        self.num_tasks = len(self.task_set)
+        self.utilization = self.calculate_utilization()
+
+        if task.L == Level.LO: self.lo_tasks_list.append(task)
+        if task.L == Level.HI: self.hi_tasks_list.append(task)
+        
+        if target_u - 0.005 <= self.utilization <= target_u + 0.005 \
+          and len(self.lo_tasks_list) > 0 and len(self.hi_tasks_list) > 0 and self.calculate_t_max() > 0:
+          break
+
+        elif self.utilization > target_u + 0.005:
+          self.task_set = {}
+          self.num_tasks = len(self.task_set)
+          self.lo_tasks_list = []
+          self.hi_tasks_list = []
+
       self.t_max = self.calculate_t_max()
       self.thm1 = None
       self.thm2 = None
       self.thm3 = None
-      self.lo_tasks_list = [task for task in self.task_set.values() if task.L == Level.LO]
-      self.hi_tasks_list = [task for task in self.task_set.values() if task.L == Level.HI]
+
     else:
       self.num_tasks = task_set_dict["num_tasks"]
       self.t_max = task_set_dict["t_max"]
@@ -90,7 +98,7 @@ class Task_Set:
       self.lo_tasks_list = task_set_dict["lo_tasks_list"]
       self.hi_tasks_list = task_set_dict["hi_tasks_list"]
       self.task_set = {task.ID : task for task in self.lo_tasks_list + self.hi_tasks_list}
-
+  
   def __str__(self):
     task_set_string = "Task set: {} tasks, t_max = {}, utilization = {} \n".format(self.num_tasks, self.t_max, self.utilization)
     if self.thm1 is not None: task_set_string += "Thm 1 -- {}\n".format(self.thm1)
@@ -99,7 +107,7 @@ class Task_Set:
     for task in self.task_set.values():
       task_set_string += str(task) + "\n"
     return task_set_string
-  
+
   def __repr__(self):
     lo_tasks = str([repr(task) for task in self.lo_tasks_list])
     hi_tasks = str([repr(task) for task in self.hi_tasks_list])
@@ -116,26 +124,6 @@ class Task_Set:
           """.format(self.num_tasks, self.t_max, self.utilization, self.thm1, self.thm2, self.thm3,
                       lo_tasks.replace("'", ""), hi_tasks.replace("'", ""))
   
-  def generate_kato_utilizations(self, target_u):
-    utilizations = []
-    total_u = 0
-
-    while total_u < target_u:
-      current_u = r.uniform(MIN_U, MAX_U)
-      if current_u + total_u > target_u:
-        current_u = target_u - total_u
-      total_u += current_u
-      utilizations.append(current_u)
-
-    return utilizations
-
-  def initialize_task_set(self, target_u):
-    utilizations = self.generate_kato_utilizations(target_u)
-    return {i: generate_task(u, i) for u, i in zip(utilizations, range(len(utilizations)))}
-  
-  def calculate_utilization(self):
-    return sum([task.C_LO / task.T for task in self.task_set.values()])
-  
   def lcmT_maxD(self):
     return reduce(lambda x, y: (x * y)//gcd(x, y), [task.T for task in self.task_set.values()]) + max([task.D for task in self.task_set.values()])
 
@@ -145,17 +133,10 @@ class Task_Set:
   def calculate_t_max(self):
     return min(self.lcmT_maxD(), self.U_maxTD()) if self.U_maxTD() >= 0 else 0
 
-  def set_thm1(self, test_result):
-    self.thm1 = test_result
+  def calculate_utilization(self):
+    lo_utilization = sum([task.C_LO / task.T for task in self.task_set.values() if task.L == Level.LO])
+    hi_utilization = sum([task.C_HI / task.T for task in self.task_set.values() if task.L == Level.HI])
 
-  def set_thm2(self, test_result):
-    self.thm2 = test_result
-
-  def set_thm3(self, test_result):
-    self.thm3 = test_result
-
-def generate_valid_task_set(target_u):
-  while True:
-    task_set = Task_Set(target_u)
-    if task_set.t_max > 0:
-      return task_set
+    return lo_utilization + hi_utilization
+  
+print(Task_Set(0.5))
--- test_generate_tasks.py
+++ test_generate_tasks.py
@@ -8,28 +8,16 @@ def get_seed():
   return 70
 
 @pytest.fixture
-def min_u():
-  return 0.02
-
-@pytest.fixture
-def max_u():
-  return 0.25
-
-@pytest.fixture
-def utilization(min_u, max_u):
-  return r.uniform(min_u, max_u)
-
-@pytest.fixture
 def random_target_u():
-  return r.choice([0.5, 0.6, 0.7, 0.8, 0.9])
+  return r.choice([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
 @pytest.fixture
 def get_seed():
   return 70
 
 @pytest.fixture
-def random_task(utilization):
-  return gt.generate_task(utilization)
+def random_task():
+  return gt.generate_task()
 
 @pytest.fixture
 def random_task_set(random_target_u):
@@ -47,15 +35,6 @@ def task_set():
                    'hi_tasks_list': [gt.Task(1, 122, 23, 46, 312, 312), gt.Task(2, 108, 18, 37, 107, 107), gt.Task(4, 132, 15, 50, 233, 233)]}
   
   return gt.Task_Set(task_set_dict = task_set_dict)
-
-@pytest.fixture
-def unconstrained_task_set(random_target_u):
-  while True:
-    unconstrained_task_set = gt.Task_Set(random_target_u)
-    if all([task.D > task.T for task in unconstrained_task_set.task_set.values()]):
-      break
-
-  return unconstrained_task_set
 
 def test_canary():
   assert True
@@ -81,62 +60,37 @@ def test_C_HI_is_C_LO_when_L_is_LO():
 
   assert gt.generate_C_HI(C_LO, gt.Level.LO) == C_LO
 
-def test_C_HI_greater_than_10_inclusive():
-  C_LO = gt.generate_C_LO()
-
-  assert gt.generate_C_HI(C_LO, gt.Level.HI) >= 10
-
-def test_C_HI_less_than_100_inclusive():
+def test_C_HI_gt_10_lt_100_inclusive():
   C_LO = gt.generate_C_LO()
   
-  assert gt.generate_C_HI(C_LO, gt.Level.HI) <= 100
+  assert 10 <= gt.generate_C_HI(C_LO, gt.Level.HI, 4) <= C_LO * 4
 
 def test_C_HI_is_57(get_seed):
   r.seed(get_seed)
 
-  assert gt.generate_C_HI(25, gt.Level.HI) == 57
+  assert gt.generate_C_HI(25, gt.Level.HI, 4) == 57
 
-def test_D_greater_than_C_HI_inclusive():
+def test_D_gt_C_HI_lt_500_inclusive():
   C_HI = gt.generate_C_HI(gt.generate_C_LO(), gt.Level.HI)
   
-  assert gt.generate_D(C_HI) >= C_HI
+  assert C_HI <= gt.generate_D(C_HI) <= 500
 
-def test_D_less_than_500_inclusive():
-  C_HI = gt.generate_C_HI(gt.generate_C_LO(), gt.Level.HI)
-  
-  assert gt.generate_D(C_HI) <= 500
-
-def test_T_minimum_is_100(max_u):
-  assert gt.generate_T(5, max_u) == 20
-
-def test_T_greater_than_C_HI(max_u):
+def test_T_gt_C_HI_lt_500_inclusive():
   C_LO = gt.generate_C_LO()
   C_HI = gt.generate_C_HI(C_LO, gt.Level.HI)
   
-  assert gt.generate_T(C_LO, max_u) >= C_HI
+  assert C_HI <= gt.generate_T(C_HI) <= 500
 
 def test_task_C_LO_less_than_C_HI_inclusive(random_task):
   assert random_task.C_LO < random_task.C_HI if random_task.L == gt.Level.HI else random_task.C_LO == random_task.C_HI
 
+def test_task_C_HI_less_than_D_inclusive(random_task):
+  assert random_task.C_HI <= random_task.D
+
 def test_task_C_HI_less_than_T_inclusive(random_task):
   assert random_task.C_HI <= random_task.T 
 
-def test_task_max_C_HI_equal_min_T(random_task, max_u):
-  random_task.C_HI = 4 * random_task.C_LO
-  random_task.T = gt.generate_T(random_task.C_LO, max_u)
-
-  assert random_task.C_HI == random_task.T
-
 ''' TEST TASK SET METHODS '''
-
-def test_utilizations_greater_than_0_exclusive(random_task_set, random_target_u):
-  assert min(random_task_set.generate_kato_utilizations(random_target_u)) > 0
-
-def test_utilizations_less_than_maxu_inclusive(random_task_set, random_target_u, max_u):
-  assert max(random_task_set.generate_kato_utilizations(random_target_u)) < max_u + 0.00001
-
-def test_utilizations_sum_equal_target_sum(random_task_set, random_target_u):
-  assert sum(random_task_set.generate_kato_utilizations(random_target_u)) == pytest.approx(random_target_u)
 
 def test_task_set_utilization_approximately_target_u(random_task_set, random_target_u):
   assert random_target_u - random_task_set.utilization < 0.05
@@ -151,26 +105,23 @@ def test_t_max_returns_261(task_set):
   assert task_set.calculate_t_max() == 261
 
 def test_t_max_is_positive(random_task_set):
-  assert random_task_set.calculate_t_max() >= 0
-
-def test_t_max_is_zero_unconstrained_task_set(unconstrained_task_set):
-  assert unconstrained_task_set.calculate_t_max() == 0
+  assert random_task_set.calculate_t_max() > 0
 
 ''' TEST TASK SET '''
+
 def test_task_set_not_empty(random_task_set):
   assert random_task_set.num_tasks > 0
+
+def test_task_set_num_tasks_is_length(random_task_set):
+  assert random_task_set.num_tasks == len(random_task_set.task_set)
+
+def test_task_set_t_max_pos(random_task_set):
+  assert random_task_set.t_max > 0
 
 def test_task_set_lo_is_lo(random_task_set):
   assert all([task.L == gt.Level.LO for task in random_task_set.lo_tasks_list])
 
 def test_task_set_hi_is_hi(random_task_set):
   assert all([task.L == gt.Level.HI for task in random_task_set.hi_tasks_list])
-
-''' TEST VALID TASK SETS GENERATOR '''
-
-def test_valid_task_sets_t_max_greater_0():
-  task_set = gt.generate_valid_task_set(0.5)
-
-  assert task_set.t_max > 0
 
 
