41bf66f9ec066725082f962259857747840756cc
dotcommadotcom <byjiwoolee@gmail.com>
2023-05-23 17:16:17 -0500

    edit batch_search

--- batch_search.py
+++ batch_search.py
@@ -14,7 +14,7 @@ def run_search_algorithm(dataframe):
     task_set_dict = ch.parse_csv_row_to_dict(row)
     task_set = gt.Task_Set(task_set_dict=task_set_dict)
     start_time = time.time()
-    task_set_dict["search_result"], task_set_dict["tight_task_set"] = alg.tighten_deadline(task_set)
+    task_set_dict["search_result"], task_set_dict["search_task_set"] = alg.tighten_deadline(task_set)
     end_time = time.time()
     task_set_dict["search_time"] = end_time - start_time
     post_tight_results.append(task_set_dict)
@@ -24,7 +24,7 @@ def run_search_algorithm(dataframe):
   for i, row in post_tight_df.iterrows():
     post_tight_df.loc[i, "lo_tasks_list"] = str(post_tight_df.loc[i, "lo_tasks_list"])
     post_tight_df.loc[i, "hi_tasks_list"] = str(post_tight_df.loc[i, "hi_tasks_list"])
-    post_tight_df.loc[i, "tight_task_set"] = repr(post_tight_df.loc[i, "tight_task_set"])
+    post_tight_df.loc[i, "search_task_set"] = repr(post_tight_df.loc[i, "search_task_set"])
 
   return post_tight_df
 
@@ -42,4 +42,4 @@ if __name__ == "__main__":
       print("running utilization {} file {}".format(utilization_tag, db))
       df = ch.read_csv_to_processed_dataframe(db)
       post_tight_df = run_search_algorithm(df)
-      post_tight_df.to_csv('./db_test_post/db_{}_post.csv'.format(utilization_tag), header = True, index = False)
+      post_tight_df.to_csv('./db_test_search/db_{}_search.csv'.format(utilization_tag), header = True, index = False)
