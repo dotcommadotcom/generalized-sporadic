db23ab5f7d0accd023eaea0fced84593ab93010a
Adam Stewart <adam.stewart@cnhind.com>
2023-10-09 21:47:34 +0900

    add main and makefile

---
+++ Makefile
@@ -0,0 +1,25 @@
+CC = clang++
+CFLAGS = -Xclang -fopenmp -std=c++17 -I/opt/homebrew/opt/libomp/include
+LDFLAGS = -L/opt/homebrew/opt/libomp/lib
+LIBS = -lomp
+
+SRC = main.cpp
+TARGET = main
+
+COUNT = 100
+UTILIZATION = 0.5
+
+all: $(TARGET)
+
+$(TARGET): $(SRC)
+	$(CC) $(CFLAGS) $(LDFLAGS) $(LIBS) $^ -o $@
+
+run: $(TARGET)
+	./$(TARGET) $(COUNT) $(UTILIZATION)
+
+clean:
+	rm -f $(OBJS) $(TARGET)
+
+run-all: all run clean
+
+.PHONY: all clean
---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <cassert>
+
+#include "./src/search_algorithm.h"
+
+void write_result_to_csv(const string& filename, const vector<int>& test_result) {
+  ofstream file(filename, ios::app);
+  if (!file.is_open()) {
+    cerr << "Failed to open file: " << filename << endl;
+    return;
+  }
+
+  for (const auto& cell : test_result) {
+    file << cell;
+    if (&cell != &test_result.back()) {
+      file << ","; 
+    }
+  }
+  file << "\n"; 
+  file.close();
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 3){
+    std::cout << "Error: Provide count and utilization tag." << std::endl;
+    return 1;
+  }
+
+  int count = stoi(argv[1]);
+  double utilization = stod(argv[2]);
+  int before_success = 0, after_eds_success = 0, after_naive_success = 0;
+  vector<int> result;
+  int cum_duration = 0, cum_duration2 = 0; 
+  int cum_eds_duration = 0, cum_naive_duration = 0;
+
+  for (int i = 0; i < count; ++i) {
+    TaskSet task_set_eds = TaskSet(utilization);
+    task_set_eds.set_thm1(schedulability_test_thm1_parallel(task_set_eds));
+    task_set_eds.set_thm2(schedulability_test_thm2(task_set_eds));
+    task_set_eds.set_thm3(schedulability_test_thm3(task_set_eds));
+    TaskSet task_set_naive = task_set_eds;
+    assert(&task_set_eds != &task_set_naive);
+
+    if (task_set_eds.get_thm1() && task_set_eds.get_thm2() && task_set_eds.get_thm3()) {
+      before_success++;
+    }
+
+    // Enhanced Deadline Search Algorithm
+    auto start_time = chrono::high_resolution_clock::now();
+    if (!is_eligible(task_set_eds) || deadline_search_algorithm(task_set_eds).first == "Success") {
+      after_eds_success++;
+    } 
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+    cum_eds_duration += duration.count();
+
+    // Naive Algorithm
+    start_time = chrono::high_resolution_clock::now();
+    if (!is_eligible(task_set_naive) || naive_algorithm(task_set_naive).first == "Success") {
+      after_naive_success++;
+    } 
+    end_time = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+    cum_naive_duration += duration.count();
+  }
+
+  result.push_back(count);
+  result.push_back(before_success);
+  result.push_back(after_eds_success);
+  result.push_back(cum_eds_duration);
+  result.push_back(after_naive_success);
+  result.push_back(cum_naive_duration);
+
+  write_result_to_csv(to_string(count) + ".csv", result);
+}
